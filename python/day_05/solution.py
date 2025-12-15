e9cf350fc1df12b65a988f1c1474d205d3039b73
Felix Soubelet <felix.soubelet@protonmail.com>
2025-12-15 11:00:44 +0100

    naive very long part 2 solution

--- python/day_05/solution.py
+++ python/day_05/solution.py
@@ -46,12 +46,32 @@ So, in this example, 3 of the available ingredient IDs are fresh.
 
 Process the database file from the new inventory management system.
 How many of the available ingredient IDs are fresh?
+
+--- Part Two ---
+
+The Elves start bringing their spoiled inventory to the trash chute at the back of the kitchen.
+
+So that they can stop bugging you when they get new inventory, the Elves would like to know all of the IDs that the fresh ingredient ID ranges consider to be fresh.
+An ingredient ID is still considered fresh if it is in any range.
+
+Now, the second section of the database (the available ingredient IDs) is irrelevant.
+Here are the fresh ingredient ID ranges from the above example:
+
+3-5
+10-14
+16-20
+12-18
+
+The ingredient IDs that these ranges consider to be fresh are 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, and 20.
+So, in this example, the fresh ingredient ID ranges consider a total of 14 ingredient IDs to be fresh.
+
+Process the database file again.
+How many ingredient IDs are considered to be fresh according to the fresh ingredient ID ranges?
 """
 
 from __future__ import annotations
 
 from pathlib import Path
-from pprint import pprint  # noqa: F401
 
 DAY_DIR: Path = Path(__file__).parent
 INPUTS: Path = DAY_DIR / "input.txt"
@@ -132,19 +152,41 @@ def solve_part1(inputs: list[str]) -> int:
 
 def solve_part2(inputs: list[str]) -> int:
     """
-    Solves part 2.
+    Solves part 2. We parse the inputs into the ranges of fresh
+    ingredient IDs and the available ingredient IDs, the latter
+    is discarded for this part.
+
+    We then go over the ranges and count how many IDs sit within
+    the fresh ranges. We must be wary not to count an ingredient
+    ID more than once if it falls into multiple ranges.
 
     Parameters
     ----------
-    inputs : list[str]
     inputs : list[str]
         The input lines representing the ranges and IDs.
 
     Returns
     -------
     int
+        The number of ingredient IDs that are considered fresh
+        according to the fresh ranges.
     """
-    return 0
+    all_valid_ids: list[int] = []
+    fresh_ranges, _ = get_ranges_and_ids(inputs)
+    print(len(fresh_ranges))
+
+    # We go over each range and count the number of IDs in it
+    for range_start, range_end in fresh_ranges:
+        print(f"Range ({range_start}-{range_end}) with length {range_end - range_start + 1}")
+        for id in range(range_start, range_end + 1):
+            all_valid_ids.append(id)
+
+    # Now we might have added the same ID multiple times, so
+    # we convert to a set to get the unique IDs only
+    unique_valid_ids: set[int] = set(all_valid_ids)
+
+    # We return the number of unique valid IDs
+    return len(unique_valid_ids)
 
 
 # ----- Running ----- #
@@ -153,9 +195,9 @@ if __name__ == "__main__":
     inputs: list[str] = INPUTS.read_text().splitlines()
 
     # print(solve_part1(EXAMPLE.read_text().splitlines()))
-    solution1 = solve_part1(inputs)
-    print(f"Part 1 answer: {solution1}")
+    # solution1 = solve_part1(inputs)
+    # print(f"Part 1 answer: {solution1}")
 
     # print(solve_part2(EXAMPLE.read_text().splitlines()))
-    # solution2 = solve_part2(inputs)
-    # print(f"Part 2 answer: {solution2}")
+    solution2 = solve_part2(inputs)
+    print(f"Part 2 answer: {solution2}")
